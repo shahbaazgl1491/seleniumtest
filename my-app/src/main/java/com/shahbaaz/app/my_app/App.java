15886b920c0631065f48949d2b764dfe345c44ed
Shahbaaz Siddiqui <shahbaaz.siddiqui@bmo.com>
2018-11-12 18:18:44 -0500

    commit 1

---
+++ my-app/src/main/java/com/shahbaaz/app/my_app/App.java
@@ -0,0 +1,17 @@
+package com.shahbaaz.app.my_app;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!xxxxxxxxx" );
+
+        System.out.println( "Hello World!xxxxxxxxx" );
+         
+        
+    }
+}
